b1266b59b0d072113081681a3d749edcb7cb9231
sson127127@gmail.com <sson127127@gmail.com>
2023-03-28 14:37:21 +0900

    초기 커밋.

---
+++ 4_5_호텔예약시스템/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class HotelBookingSystem
+{
+    static void Main(string[] args)
+    {
+
+    }
+    
+    private string[] availableRooms;
+
+    public string GuestName { get; set; }
+    public string GuestPhoneNum { get; set; }
+    public int BookingNum { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+    public string RoomNum { get; set; }
+
+    public HotelBookingSystem()
+    {
+        availableRooms = new string[] { "101", "102", "103", "201", "202", "203" };
+    }
+
+    public bool VerifyGuest()
+    {
+        Console.WriteLine("예약자 이름 : " + GuestName);
+        Console.WriteLine("예약자 휴대폰번호 : " + GuestPhoneNum);
+
+        return true;
+    }
+
+    public int CheckAvailableRooms()
+    {
+        int count = availableRooms.Length;
+        Console.WriteLine("예약 가능한 객실 개수 : " + count);
+        return count;
+    }
+}
